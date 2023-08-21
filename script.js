701962735b1d74d9ed04e3fa88fa51ecfa66d816
Tsaone <tsaoneb3@gmail.com>
2023-08-21 05:22:50 +0200

    Made photo to be draggable and droppable by JS

---
+++ script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll(".box"),
+image = document.querySelector(".image");
+
+boxes.forEach((box) => {
+    box.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        box.classList.add("hovered");
+    });
+
+box.addEventListener("dragleave", () => {
+    box.classList.remove("hovered");
+});
+
+box.addEventListener("drop", () => {
+    box.appendChild(image);
+    box.classList.remove("hovered");
+})
+
+});
